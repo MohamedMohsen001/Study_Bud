d17a86e505c2a6d03700210fd42cf387184bb983
Mohamed Mohsen <marcspecter2091@gmail.com>
2025-07-09 03:09:32 +0300

    added files

--- StudyBud/base/urls.py
+++ StudyBud/base/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('room/<str:pk>/', views.room, name="room"),
+    path('create-room/', views.CreateRoom, name="create-room"),
+    path('update-room/<str:pk>/', views.UpdateRoom, name="update-room"),
+    path('delete-room/<str:pk>/', views.DeleteRoom, name="delete-room"),
 ]
